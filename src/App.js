4951f1239438efadcc2992b3f2a1ad214adfdaf4
Zahra El Baba <zahrababa73@hotmail.com>
2025-02-12 21:14:14 +0200

    about page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import HomePage from "./components/HomePage";
 import Contact from "./components/Contact";
 import Offers from "./components/Offers"; 
+import About from "./components/About";
 
 const App = () => {
   return (
@@ -12,7 +13,7 @@ const App = () => {
       <Navbar />  {/* Navbar is placed here to be visible on all pages */}
       <Routes>
         <Route path="/" element={<HomePage />} />  HomePage route
-        {/* <Route path="/about" element={<AboutPage />} /> */}
+        <Route path="/about" element={<About />}></Route>
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Offers" element={<Offers />} /> {/* Add this line */}
       </Routes>
